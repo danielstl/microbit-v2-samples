5a27ed7d1cce8aaaf2f4f485707d59c06c0684f5
danielstl <15856061+danielstl@users.noreply.github.com>
2022-09-05 16:25:07 +0100

    attempt to read gps data from i2c

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,63 @@
 #include "MicroBit.h"
+#include "MicroBitDevice.h"
 
 MicroBit uBit;
 
-int 
-main()
+struct GPGGAMessage {
+    uint16_t utc;
+    double lat;
+    uint8_t lat_dir;
+    double lon;
+    uint8_t lon_dir;
+    uint8_t quality;
+    uint8_t sats;
+    float hdop;
+    double alt;
+    uint8_t a_units;
+    float undulation;
+    uint8_t u_units;
+    uint8_t age;
+};
+
+int main()
 {
     uBit.init();
+    //uBit.serial.init();
 
-    while(1)
-        uBit.display.scroll("HELLO WORLD!");
+    //uBit.log.clear();
+
+    //ManagedBuffer buffer = ManagedBuffer(255);
+    int index = 0;
+
+    //uBit.i2c.clearBus();
+
+    while (1) {
+        uBit.sleep(100);
+
+        uint8_t* res = new uint8_t[2];
+        int resCode = uBit.i2c.readRegister(0x42, 0xfd, res, 1, true);
+        int resCode2 = uBit.i2c.readRegister(0x42, 0xfe, res + 1, 1, true);
+
+
+        //buffer.setByte(index, res);
+
+        /*if (res < 255) {
+            if (res == 10) {
+                auto str = ManagedString(buffer).toCharArray();
+                DMESGF(str);
+
+                if (strncmp(str, "$GPGGA", 5) == 0) {
+
+
+                }
+            }
+        }*/
+
+        //uBit.sleep(1000);
+        //uint8_t* data = (uint8_t*) malloc(100);
+        //uBit.i2c.read(0x42, data, 100);
+        DMESG("%c", res);
+
+        index++;
+    }
 }
