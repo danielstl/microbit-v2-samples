ceb4fca32b7985a755fc3972ab37b605daad5b9d
danielstl <15856061+danielstl@users.noreply.github.com>
2022-09-08 16:15:32 +0100

    Extract fields from GNNGA message

--- source/main.cpp
+++ source/main.cpp
@@ -22,11 +22,48 @@ struct GNGGAMessage {
 void parseGNGGAMessage(char* message) {
     if (strncmp(message, "$GNGGA", 5) == 0) {
         DMESGF("new gngga: %s", message);
+    } else {
+        return;
     }
 
-    int index = 6;
+    int len = strlen(message);
 
+    int fieldStartIndexes[15] = {};
 
+    int fieldNum = 1;
+
+    fieldStartIndexes[0] = 0;
+
+    for (int index = 0; index < len; index++) {
+        if (message[index] == ',') {
+            fieldStartIndexes[fieldNum++] = index + 1;
+            message[index] = '\0';
+        }
+
+        if (fieldNum == 14) {
+            break; // don't continue parsing after this point, we should have all our fields
+        }
+    }
+
+    if (fieldNum != 14) {
+        DMESG("Invalid GNGGA message, too few fields");
+        return;
+    }
+
+    char* utc = message + fieldStartIndexes[1];
+    char* lat = message + fieldStartIndexes[2];
+    char* lat_dir = message + fieldStartIndexes[3];
+    char* lon = message + fieldStartIndexes[4];
+    char* lon_dir = message + fieldStartIndexes[5];
+    char* quality = message + fieldStartIndexes[6];
+    char* sats = message + fieldStartIndexes[7];
+    char* hdop = message + fieldStartIndexes[8];
+    char* alt = message + fieldStartIndexes[9];
+    char* a_units = message + fieldStartIndexes[10];
+    char* undulation = message + fieldStartIndexes[11];
+    char* u_units = message + fieldStartIndexes[12];
+    char* age = message + fieldStartIndexes[13];
+    char* station = message + fieldStartIndexes[14];
 }
 
 int main()
